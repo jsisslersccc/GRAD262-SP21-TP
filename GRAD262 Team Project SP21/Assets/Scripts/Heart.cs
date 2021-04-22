c8b74f76e4a67288dced1fb96ad467eeeab2dac2
diefortheswarm <82173027+diefortheswarm@users.noreply.github.com>
2021-04-22 15:42:40 -0400

    Green Potion 3

---
+++ GRAD262 Team Project SP21/Assets/Scripts/Heart.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class Heart : MonoBehaviour
+{
+
+    public int heartValue = 1;
+    public bool htaken = false;
+    public GameObject explosion;
+
+    // if the player touches the coin, it has not already been taken, and the player can move (not dead or victory)
+    // then take the coin
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.tag == "Player") && (!htaken))
+        {
+            // mark as taken so doesn't get taken multiple times
+            htaken = true;
+
+            // if explosion prefab is provide, then instantiate it
+            if (explosion)
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+            }
+
+            // do the player collect coin thing
+            Manager.gm.CollectHeart(heartValue);
+
+            // destroy the coin
+            DestroyObject(this.gameObject);
+        }
+    }
+
+}
